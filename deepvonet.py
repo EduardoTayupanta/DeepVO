2a08e536c307150dd0c972bd1fc749b8d24d0a9d
etayupanta <wetayupanta@gmail.com>
2020-06-30 22:37:55 -0500

    Initial commit

---
+++ deepvonet.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created by etayupanta at 6/30/2020 - 21:10
+PyCharm - DeepVO
+__author__ = 'Eduardo Tayupanta'
+__email__ = 'etayupanta@yotec.tech'
+"""
+
+
+# Import Libraries:
+
+
+def main():
+    print("Hola!")
+
+
+if __name__ == "__main__":
+    main()
